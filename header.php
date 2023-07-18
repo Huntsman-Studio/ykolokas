2ca7d8c7138c18cb677ac8e0f36d182bc17f64fa
Kristel Lamaj <kri.lamaj@gmail.com>
2023-07-18 12:19:40 +0300

    Main structure index / header / footer

---
+++ header.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Containf the header.
+ */
+?>
+<!DOCTYPE HTML>
+<html <?php language_attributes(); ?>>
+    <head>
+        <meta chraset="<?php bloginfo('charset') ?>">
+        <meta name="viewport" content="width=device, initial-scale=1">
+
+        <?php wp_head(); ?>
+    </head>
+    <body <?php body_class(); ?>>
+        
