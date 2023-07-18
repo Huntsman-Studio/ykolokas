f2104f236b532c6a00c632466a31103715d27265
Kristel Lamaj <kri.lamaj@gmail.com>
2023-07-18 13:02:26 +0300

    Single Post and Archive Post

--- index.php
+++ index.php
@@ -7,6 +7,7 @@ get_header();
     if ( have_posts() ) : 
         while (have_posts() ) :
             the_post();
+            get_template_part('template-parts/post/content');
             // do our stuff
         endwhile;
     endif;
---
+++ single.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Single page template.
+ */
+
+get_header();
+?>
+
+<div id="primary" class="content-area">
+    <main id="main" class="sit-main">
+        <?php
+        while ( have_posts() ) : 
+            the_post();
+            // the_contenet();
+            get_template_part('template-parts/post/content');
+        endwhile;
+
+        // if comments are enabled then we can show the commenets template.
+        if ( comments_open() || get_comments_number() ) : 
+            comments_template();
+        endif;
+        ?>
+    </main>
+    <?php get_sidebar(); ?>
+</div>
+
+<?php
+get_footer();
+?>
